107d5b9944a62cefbbe92eb745835b67f811a284
Felipe Gabriel <felipe.gabi.souz@gmail.com>
2025-05-13 07:41:00 -0300

    13/05 p1

--- index.php
+++ index.php
@@ -11,61 +11,89 @@
 <body>
     <div class="container-fluid bg-dark text-white text-center p-5">
         <h1>Projeto SW2</h1>
-        <p>
-            Elaborado por Felipe Gabriel
-        </p>
+        <p>Elaborado por Felipe Gabriel</p>
     </div>
 
     <div class="container mt-5">
         <div class="row">
-
+            <!-- Administrador -->
             <div class="col-sm-4">
-                <a href='administrador.php' style="text-decoration : none"><h1 class="bg-primary text-warning">Administrador</h1></a>
-                <div class="col-sm-2">
-                <a href='administrador.php' style="text-decoration : none"><p>Cadastro</p></a>
-                <a href='administrador_pesquisa.php' style="text-decoration : none"><p>Pesquisa</p></a>
-
-                </div>
+                <a href="administrador.php" style="text-decoration: none;">
+                    <h1 class="bg-primary text-warning">Administrador</h1>
+                </a>
+                <a href="administrador_pesquisa.php" style="text-decoration: none;">
+                    <p class="btn btn-primary">Pesquisa</p>
+                </a>
                 <p class="text-primary">
-                    Utilize essa opção para realizar o cadastro de novos administradores, ou edita-los.
+                    Utilize essa opção para realizar o cadastro de novos administradores, ou editá-los.
                 </p>
             </div>
 
+            <!-- Categorias -->
             <div class="col-sm-4">
-                <a href='categoria.php' style="text-decoration : none"><h1 class="bg-danger text-white">Categorias</h1></a>
+                <a href="categoria.php" style="text-decoration: none;">
+                    <h1 class="bg-danger text-white">Categorias</h1>
+                </a>
+                <a href="categoria_pesquisa.php" style="text-decoration: none;">
+                    <p class="btn btn-danger">Pesquisa</p>
+                </a>
                 <p class="text-danger">
-                    Utilize essa opção para realizar o cadastro das categorias do produtos que serão vendidos online, ou edita-las.
+                    Utilize essa opção para realizar o cadastro das categorias dos produtos que serão vendidos online, ou editá-las.
                 </p>
-                <a href='categoria.php' style="text-decoration : none"><p>Cadastro</p></a>
-                <a href='categoria_pesquisa.php' style="text-decoration : none"><p>Pesquisa</p></a>
             </div>
-                
 
+            <!-- Fornecedor -->
             <div class="col-sm-4">
-                <a href='fornecedor.php'  style="text-decoration : none"><h1 class="bg-success text-danger">Fornecedor</h1></a>
+                <a href="fornecedor.php" style="text-decoration: none;">
+                    <h1 class="bg-success text-danger">Fornecedor</h1>
+                </a>
+                <a href="fornecedor_pesquisa.php" style="text-decoration: none;">
+                    <p class="btn btn-success">Pesquisa</p>
+                </a>
                 <p class="text-success">
-                    Utilize essa opção para realizar o cadastro dos fornecedores dos produtos que serão vendidos online, ou edita-las.
+                    Utilize essa opção para realizar o cadastro dos fornecedores dos produtos que serão vendidos online, ou editá-los.
                 </p>
-                <a href='fornecedor.php' style="text-decoration : none"><p>Cadastro</p></a>
-                <a href='fornecedor_pesquisa.php' style="text-decoration : none"><p>Pesquisa</p></a>
+            </div>
+        </div>
+
+        <div class="row mt-4">
+            <!-- Produtos -->
+            <div class="col-sm-4">
+                <a href="produto.php" style="text-decoration: none;">
+                    <h1 class="bg-warning">Produtos</h1>
+                </a>
+                <p class="text-warning">
+                    Use essa opção para cadastrar os produtos que serão vendidos no site.
+                </p>
             </div>
 
-        </div>
-
-        <div class="row">
+            <!-- Vendas -->
             <div class="col-sm-4">
-                <a href='produto.php'  style="text-decoration : none"><h1 class="bg-warning">Produtos</h1></a>
-                <p class="text-warning">Use essa opção para cadastrar os produtos que serão vendidos no site</p>
-        </div>
-        <div class="col-sm-4">
-            <h1 class="bg-secondary">Vendas</h1>
-            <p class="text-secondary">Use essa opção para consultar as vendas realizadas pelos clientes</p>
-        </div>
-        <div class="col-sm-4">
-        <a href='formapagamento.php'  style="text-decoration : none"><h1 class="bg-dark text-light">Forma de Pagamento</h1></a>
-            <p class="text-secondary">Use essa opção definir a sua forma de pagamento</p>
+                <h1 class="bg-secondary">Vendas</h1>
+                <p class="text-secondary">
+                    Use essa opção para consultar as vendas realizadas pelos clientes.
+                </p>
+            </div>
+
+            <!-- Forma de Pagamento -->
+            <div class="col-sm-4">
+                <a href="formapagamento.php" style="text-decoration: none;">
+                    <h1 class="bg-dark text-light">Forma de Pagamento</h1>
+                </a>
+                <p class="text-secondary">
+                    Use essa opção para definir a sua forma de pagamento.
+                </p>
+            </div>
+            <!-- Login -->
+            <div class="col-sm-4">
+                <a href="login.php" style="text-decoration: none;">
+                    <h1 class="bg-success text-light">Login</h1>
+                </a>
+                <p class="text-secondary">
+                    Use para acessar o sistema.
+                </p>
+            </div>
         </div>
     </div>
-</div>
 </body>
 </html>
---
+++ login.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <title>Login</title>
+</head>
+<body>
+
+    <div>
+        <div>
+
+            <form action="login_processa.php" method="post">
+                <p>
+                    Digite o Login:<br>
+                    <input type="text" name="login">
+                </p>
+                <p>
+                    Digite o Senha:<br>
+                    <input type="password" name="login">
+                </p>
+                <p>
+                    <input type="submit" name="Fazer login">
+                </p>
+            </form>
+
+        </div>
+    </div>
+    
+</body>
+</html>
